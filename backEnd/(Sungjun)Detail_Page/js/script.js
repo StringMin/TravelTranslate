0c48779f8594807030b97e968a9019afc8f038fc
KSJ <dmlwh34@naver.com>
2023-09-25 01:50:56 +0900

    html은 거의 진행됨. js는 공부좀 해야할듯.....

---
+++ backEnd/(Sungjun)Detail_Page/js/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const messageDiv = document.getElementById('messageDiv');
+    const showMessageBtn = document.getElementById('showMessageBtn');
+
+    showMessageBtn.addEventListener('click', function() {
+        messageDiv.textContent = 'Hello, World!';
+    });
+});
